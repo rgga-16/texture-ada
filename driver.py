07cb6d0b438e4c82e0fdec52b41f3b4ddd9d549c
rgee3894 <rgga16@gmail.com>
2020-07-17 15:06:02 +0800

    Added VGG16 model (weights in .gitignore)

--- driver.py
+++ driver.py
@@ -1,7 +1,17 @@
 import torch
 import torchvision
-
+from torchsummary import summary
+import models
 
 
 if __name__ == "__main__":
     print("Main Driver")
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    vgg16 = models.VGG16().to(device)
+
+    inp = torch.randn(1,3,244,244).to(device)
+    output = vgg16(inp)
+    
+    for layer, feature in output.items():
+        print(feature.size())
+    
