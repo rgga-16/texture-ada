23fccd93c5ed8f516c6a5cc742a573d3a9d53768
rgee3894 <rgga16@gmail.com>
2020-09-23 09:53:27 +0800

    added tests sets incl. inputs and outputs.

--- driver.py
+++ driver.py
@@ -26,84 +26,74 @@ import os
 IMSIZE=256
 
 datapath = './data'
-datatype='/images'
-furniture='/chairs'
+datatype='images'
+furniture='chairs'
 
-generic='/generic/armchair.jpeg'
+generic='generic/sofa-2.jpeg'
 
+style='selected_styles'
 
 des_furn = [
-    '/cobonpue/chair-2.jpg',
-    '/cobonpue/chair-3.jpg',
-    '/tiotuico/table-1.jpg',
-    '/milo_naval/sofa-1.jpg'
+    'cobonpue/chair-2.jpg',
+    'budji_layug/chair-1.jpg',
+    'vito_selma/chair-3.jpg'
 ]
 
 if __name__ == "__main__":
     print("Main Driver")
 
+    content_path = os.path.join(datapath,datatype,furniture,generic)
+
+    style_dir = os.path.join(datapath,datatype,style)
+
+    style_paths = [os.path.join(style_dir,fil) for fil in os.listdir(style_dir)]
     
-
     device = utils.setup_device(use_gpu = True)
-
-    # Setup model. Use pretrained VGG-19
     model = models.vgg19(pretrained=True).features.to(device).eval()
 
-    content_path = './data/images/chairs/generic/armchair.jpeg'
-    style_path = './data/images/chairs/cobonpue/chair-2.jpg'
+    save_path = content_path
+    i=0
+    for style_path in style_paths:
 
-    ## Get mask by segmenting the content image via user input
-    _,mask_path = seg.segment_points(content_path,device=device)
-    # mask = torch.from_numpy(mask).type(torch.float32)
-    # w,h,c = mask.shape
-    # mask = mask.view(c,w,h).unsqueeze(0).to(device)
-    # mask = F.interpolate(mask,size=(IMSIZE,IMSIZE))
+        _,mask_path = seg.segment_points(save_path,device=device)
+        mask_img = utils.load_image(mask_path)
+        mask = utils.image_to_tensor(mask_img,image_size=IMSIZE).to(device)
+        
+        _,style_mask_path = seg.segment_points(style_path,device=device)
+        style_mask_img = utils.load_image(style_mask_path)
+        style_mask = utils.image_to_tensor(style_mask_img,image_size=IMSIZE).to(device)
 
-    # # mask = utils.image_to_tensor(mask).to(device)
+        print("Mask shape: {}".format(mask.shape))
+        print("Style mask shape: {}".format(style_mask.shape))
 
-    ## Or get mask by loading from path
-    mask_img = utils.load_image(mask_path)
-    mask = utils.image_to_tensor(mask_img,image_size=IMSIZE).to(device)
+        content_img = utils.load_image(save_path)
+        style_img = utils.load_image(style_path)
 
-    
-    _,style_mask_path = seg.segment_points(style_path,device=device)
-    # style_mask = torch.from_numpy(style_mask).type(torch.float32)
-    # w,h,c = style_mask.shape
-    # style_mask = style_mask.view(c,w,h).unsqueeze(0).to(device)
-    # style_mask = F.interpolate(style_mask,size=(IMSIZE,IMSIZE))
+        content = utils.image_to_tensor(content_img,image_size=IMSIZE).to(device)
+        style = utils.image_to_tensor(style_img,image_size=IMSIZE).to(device)
 
-    style_mask = utils.image_to_tensor(utils.load_image(style_mask_path),image_size=IMSIZE).to(device)
+        content_clone = content.clone().detach()
 
-    print("Mask shape: {}".format(mask.shape))
-    print("Style mask shape: {}".format(style_mask.shape))
+        content = content * mask
 
-    content_img = utils.load_image(content_path)
-    style_img = utils.load_image(style_path)
+        style = style * style_mask
+        
+        print("Mask shape: {}".format(mask.shape))
+        print("content shape: {}".format(content.shape))
+        print("style shape: {}".format(style.shape))
 
-    content = utils.image_to_tensor(content_img,image_size=IMSIZE).to(device)
-    style = utils.image_to_tensor(style_img,image_size=IMSIZE).to(device)
+        # setup normalization mean and std
+        normalization_mean = torch.tensor([0.485,0.456,0.406]).to(device)
+        normalization_std = torch.tensor([0.229,0.224,0.225]).to(device)
 
-    content_clone = content.clone().detach()
+        initial = content.clone()
 
-    content = content * mask
+        output = st.run_style_transfer(model,normalization_mean,normalization_std,content,style,initial,EPOCHS=1000)
 
-    style = style * style_mask
-    
-    print("Mask shape: {}".format(mask.shape))
-    print("content shape: {}".format(content.shape))
-    print("style shape: {}".format(style.shape))
-
-    # setup normalization mean and std
-    normalization_mean = torch.tensor([0.485,0.456,0.406]).to(device)
-    normalization_std = torch.tensor([0.229,0.224,0.225]).to(device)
-
-    initial = content.clone()
-
-    output = st.run_style_transfer(model,normalization_mean,normalization_std,content,style,initial,EPOCHS=1000)
-
-    save_path = 'outputs/stylized_output.png'
-    final = (output * mask) + (content_clone * (1-mask))
-    final_img = utils.tensor_to_image(final)
-    final_img.save(save_path)
+        save_path = 'outputs/stylized_output_{}.png'.format(i+1)
+        i+=1
+        final = (output * mask) + (content_clone * (1-mask))
+        final_img = utils.tensor_to_image(final)
+        final_img.save(save_path)
 
 
