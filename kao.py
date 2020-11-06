355899603a9d7cc40360b43acefe34b411ac3e35
rgee3894 <rgga16@gmail.com>
2020-11-06 13:54:06 +0800

    Fixed kao model and implemented texture transfer

--- kao.py
+++ kao.py
@@ -1,7 +1,8 @@
 import kaolin as kal
 from kaolin.graphics import NeuralMeshRenderer as Renderer 
 from kaolin.graphics.nmr.util import get_points_from_angles
-from kaolin.datasets.modelnet import ModelNet
+from kaolin.datasets.modelnet import ModelNet 
+from kaolin.datasets.shapenet import ShapeNet
 
 
 import torch
@@ -16,6 +17,7 @@ import utils
 import losses
 import dextr.segment as s
 import style_transfer as st
+from models import VGG19
 
 import copy
 import os
@@ -27,31 +29,34 @@ from defaults import DEFAULTS as D
 
 from args import parse_arguments
 
-class Model(nn.Module):
+class MeshRendererModel(nn.Module):
 
-    def content_loss(self,output_mesh,content_mesh):
-        return
+    def __init__(self,mesh, style,
+                textures_=None,texture_size=D.TEXTURE_SIZE(),
+                camera_distance = D.CAM_DISTANCE(),
+                device=D.DEVICE()):
+        super(MeshRendererModel,self).__init__()
 
-    def __init__(self,mesh, style, args):
-        super(Model,self).__init__()
-        self.args = args
 
         self.mesh = mesh
-
-        vertices =  normalize_vertices(self.mesh.vertices).unsqueeze(0)
+        vertices =  self.normalize_vertices(self.mesh.vertices).unsqueeze(0)
         faces = self.mesh.faces.unsqueeze(0)
-
         self.register_buffer('vertices',vertices)
         self.register_buffer('faces',faces)
         self.register_buffer('style',style)
 
-        # Initialize textures
-        textures = torch.ones(
-            1,self.faces.shape[1],
-            self.args.texture_size,self.args.texture_size,self.args.texture_size,
-            3,dtype=torch.float32,
-            device=D.DEVICE()
-        )
+        if(textures_ is not None):
+            textures = textures_
+        else:
+            # Initialize textures
+            textures = torch.ones(
+                1,self.faces.shape[1],
+                texture_size,texture_size,texture_size,
+                3,dtype=torch.float32,
+                device=device
+            )
+        
+        self.camera_distance=camera_distance
 
         self.textures = nn.Parameter(textures)
 
@@ -60,6 +65,32 @@ class Model(nn.Module):
         renderer.light_intensity_directional = 0.0
         renderer.light_intensity_ambient = 2.0
         self.renderer = renderer
+
+        self.transform = kal.transforms.Compose([
+            self.to_device,
+            kal.transforms.NormalizeMesh(),
+        ])
+
+        self.device=device
+        self.to(device)
+
+
+    """
+    Casts input to the device
+    """
+    def to_device(self,input):
+        input.to(self.device)
+        return input
+    
+    def normalize_vertices(self,vertices):
+        """
+        Normalize mesh vertices into a unit cube centered at zero.
+        """
+        vertices = vertices - vertices.min(0)[0][None, :]
+        vertices /= torch.abs(vertices).max()
+        vertices *= 2
+        vertices -= vertices.max(0)[0][None, :] / 2
+        return vertices
 
     def render_image(self, azimuth=None, elevation=None):
 
@@ -95,67 +126,68 @@ class Model(nn.Module):
             total_loss+=loss
         return total_loss
 
-def normalize_vertices(vertices):
-    """
-    Normalize mesh vertices into a unit cube centered at zero.
-    """
-    vertices = vertices - vertices.min(0)[0][None, :]
-    vertices /= torch.abs(vertices).max()
-    vertices *= 2
-    vertices -= vertices.max(0)[0][None, :] / 2
-    return vertices
 
-def to_device(input):
-    input.to(device)
-    return input
+def texture_transfer_gatys(render_model,style,
+                            descriptor=VGG19(),EPOCHS=D.EPOCHS(),
+                            style_layers = D.STYLE_LAYERS.get(),
+                            style_weight=1e6,
+                            s_layer_weights=D.SL_WEIGHTS.get()):
+    
+    optimizer = torch.optim.SGD(render_model.parameters(),lr=1e-2)
+
+    progress = tqdm(range(EPOCHS))
+    mse_loss = nn.MSELoss()
+
+
+    style_gram = st.get_features(descriptor,style,style_layers=style_layers)
+
+    for i in progress:
+
+        optimizer.zero_grad()
+        loss=0
+        rendered_img = render_model.render_image()
+        rendered_img.data.clamp_(0,1)
+
+        rendered_gram = st.get_features(descriptor,rendered_img,style_layers=style_layers)
+
+        for layer in style_layers.values():
+            diff = torch.sum((rendered_gram[layer]-style_gram[layer])**2)
+            diff *= s_layer_weights[layer]
+            loss+=diff
+        
+        loss.backward()
+        optimizer.step()
+        progress.set_description('EPOCH {} | LOSS: {}'.format(i+1,loss.item()))
+    
+    rendered_imgs = []
+    for i in tqdm(range(0,360,15)):
+        image = render_model.render_image(azimuth=i,elevation=15)
+        image.data.clamp_(0,1)
+        rendered_imgs.append(utils.tensor_to_image(image))
+    
+    utils.save_gif(rendered_imgs,filename='./outputs/renders/rendered_model.gif')
+
 
 
 def main():
 
     args = parse_arguments()
-
-    transform = kal.transforms.Compose([
-        to_device,
-        kal.transforms.NormalizeMesh()
-    ])
-
-    dataset = ModelNet(root='./data/3d-models/ModelNet10',
-                        split='train',
-                        transform=transform,
-                        categories=['chair'])
-
-    mesh = next(iter(dataset)).data
-
-    # Load style furniture image
     
-    style = utils.image_to_tensor(utils.load_image(args.image)).to(device).detach()
-
+    # Load style furniture image
+    style = utils.image_to_tensor(utils.load_image(args.style)).detach()
+    
+    mesh = utils.load_mesh(args.mesh,has_textures=True)
     
     # Create model for 3D texture transfer
-    render_model = Model(mesh,style,args).to(device)
-
-    optim = torch.optim.Adam(render_model.parameters(),lr=1e-2,betas=(0.5,0.999))
-
-    progress_bar = tqdm(range(args.epochs))
-    for i in progress_bar:
-        optim.zero_grad()
-        diff = render_model()
-        progress_bar.set_description('EPOCH {} | LOSS: {}'.format(i+1,diff.item()))
-        diff.backward()
-        optim.step()
-
-    for i in tqdm(range(0,360,45)):
-        utils.tensor_to_image(render_model.render_image(azimuth=i,elevation=-100)).save('outputs/renders/front_{}.png'.format(i))
-        utils.tensor_to_image(render_model.render_image(azimuth=i,elevation=100)).save('outputs/renders/back_{}.png'.format(i))
-
-    # initial_img = rendered_img.clone().detach()
-
-    # # Style transfer from style furn to mesh texture
-    # final = st.style_transfer_gatys2(st_model,rendered_img, style,initial_img)
+    render_model = MeshRendererModel(mesh,style,args)
     
+    texture_transfer_gatys(render_model,style)
+
+
+
+
 
 
 if __name__ == '__main__':
-    device = D.DEVICE()
     main()
     
