042b864612693a22575753f355888340e15c81b0
rgee3894 <rgga16@gmail.com>
2020-11-06 13:52:58 +0800

    Renamed and deleted some user args

--- args.py
+++ args.py
@@ -9,17 +9,15 @@ def parse_arguments():
 
     parser.add_argument('--mesh', type=str, default=D.MESH_PATH(),
                         help='Path to the mesh OBJ file')
-    parser.add_argument('--image', type=str, default=D.STYLE_PATH(),
+    parser.add_argument('--style', type=str, default=D.STYLE_PATH(),
                         help='Path to the style image to transfer texture from')
     parser.add_argument('--output_path', type=str, default='outputs',
                         help='Path to the output directory')
     parser.add_argument('--epochs', type=int, default=D.EPOCHS(),
                         help='Number of epochs to optimize')
-    parser.add_argument('--camera_distance', type=float, default=2.732,
+    parser.add_argument('--camera_distance', type=float, default=D.CAM_DISTANCE(),
                         help='Distance from camera to object center')
-    parser.add_argument('--elevation', type=float, default=0,
-                        help='Camera elevation')
-    parser.add_argument('--texture_size', type=int, default=4,
+    parser.add_argument('--texture_size', type=int, default=D.TEXTURE_SIZE(),
                         help='Dimension of texture')
 
     return parser.parse_args()
