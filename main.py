4d02b47440fb038d29f39ee223f600c187eb8ee8
rgee3894 <rgga16@gmail.com>
2020-11-18 14:23:55 +0800

    Added more input style images

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import style_transfer as st
 import utils
 import args
 
-from kao import MeshRendererModel,texture_transfer_gatys
 from args import parse_arguments
+import os
+
+from dextr.segment import segment_points
 
 from defaults import DEFAULTS as D
 
@@ -14,17 +16,37 @@ from defaults import DEFAULTS as D
 def main():
     print("Main Driver")
 
-    args = parse_arguments()
+    style_name = 'sofa-1.jpg'
+
+    style_path = os.path.join(D.STYLE_DIR.get(),style_name)
+    _,mask_filepath = segment_points(style_path,device=D.DEVICE())
+
+    style = utils.image_to_tensor(utils.load_image(style_path))
+
+    mask = utils.image_to_tensor(utils.load_image(mask_filepath))
+
+    cropped = style * mask 
+
+    final = utils.tensor_to_image(cropped)
+    final_name = style_name+'_cropped.png'
+    save_path = os.path.join(D.STYLE_DIR.get(),final_name)
+    final.save(save_path)
+
+
+
+
+
+    # args = parse_arguments()
     
     # Load style furniture image
-    style = utils.image_to_tensor(utils.load_image(args.style)).detach()
+    # style = utils.image_to_tensor(utils.load_image(args.style)).detach()
     
-    mesh = utils.load_mesh(args.mesh)
+    # mesh = utils.load_mesh(args.mesh)
     
-    # Create model for 3D texture transfer
-    render_model = MeshRendererModel(mesh,style)
+    # # Create model for 3D texture transfer
+    # render_model = MeshRendererModel(mesh,style)
     
-    texture_transfer_gatys(render_model,style)
+    # texture_transfer_gatys(render_model,style)
 
 
 
