d7dd0b0fac53e387b66655b41ab869af613489ab
rgee3894 <rgga16@gmail.com>
2021-01-12 15:21:00 +0800

    Replaced Pyramid2D batch norm layers with instnorm

--- models.py
+++ models.py
@@ -134,11 +134,11 @@ class Conv_block2D(nn.Module):
         super(Conv_block2D, self).__init__()
 
         self.conv1 = nn.Conv2d(n_ch_in, n_ch_out, 3, padding=0, bias=True)
-        self.bn1 = nn.BatchNorm2d(n_ch_out, momentum=m)
+        self.bn1 = nn.InstanceNorm2d(num_features=n_ch_out,momentum=m)
         self.conv2 = nn.Conv2d(n_ch_out, n_ch_out, 3, padding=0, bias=True)
-        self.bn2 = nn.BatchNorm2d(n_ch_out, momentum=m)
+        self.bn2 = nn.InstanceNorm2d(num_features=n_ch_out,momentum=m)
         self.conv3 = nn.Conv2d(n_ch_out, n_ch_out, 1, padding=0, bias=True)
-        self.bn3 = nn.BatchNorm2d(n_ch_out, momentum=m)
+        self.bn3 = nn.InstanceNorm2d(num_features=n_ch_out,momentum=m)
 
     def forward(self, x):
         # Pad x with its top and bottom pixel layers
@@ -154,16 +154,16 @@ class Conv_block2D(nn.Module):
         x = F.leaky_relu(self.bn3(self.conv3(x)))
         return x
 
-#Up-sampling + batch normalization block
-class Up_Bn2D(nn.Module):
+#Up-sampling + instance normalization block
+class Up_In2D(nn.Module):
     def __init__(self, n_ch):
-        super(Up_Bn2D, self).__init__()
+        super(Up_In2D, self).__init__()
 
         self.up = nn.Upsample(scale_factor=2, mode='nearest')
-        self.bn = nn.BatchNorm2d(n_ch)
+        self.inst_norm = nn.InstanceNorm2d(num_features=n_ch)
 
     def forward(self, x):
-        x = self.bn(self.up(x))
+        x = self.inst_norm(self.up(x))
         return x
 
 class Pyramid2D(nn.Module):
@@ -171,23 +171,23 @@ class Pyramid2D(nn.Module):
         super(Pyramid2D, self).__init__()
 
         self.cb1_1 = Conv_block2D(ch_in,ch_step) # ch_step=8
-        self.up1 = Up_Bn2D(ch_step)
+        self.up1 = Up_In2D(ch_step)
 
         self.cb2_1 = Conv_block2D(ch_in,ch_step)
         self.cb2_2 = Conv_block2D(2*ch_step,2*ch_step) # ch_step=16
-        self.up2 = Up_Bn2D(2*ch_step)
+        self.up2 = Up_In2D(2*ch_step)
 
         self.cb3_1 = Conv_block2D(ch_in,ch_step)
         self.cb3_2 = Conv_block2D(3*ch_step,3*ch_step) # ch_step=24
-        self.up3 = Up_Bn2D(3*ch_step)
+        self.up3 = Up_In2D(3*ch_step)
 
         self.cb4_1 = Conv_block2D(ch_in,ch_step)
         self.cb4_2 = Conv_block2D(4*ch_step,4*ch_step) # ch_step=32
-        self.up4 = Up_Bn2D(4*ch_step)
+        self.up4 = Up_In2D(4*ch_step)
 
         self.cb5_1 = Conv_block2D(ch_in,ch_step)
         self.cb5_2 = Conv_block2D(5*ch_step,5*ch_step) # ch_step=40
-        self.up5 = Up_Bn2D(5*ch_step)
+        self.up5 = Up_In2D(5*ch_step)
 
         self.cb6_1 = Conv_block2D(ch_in,ch_step)
         self.cb6_2 = Conv_block2D(6*ch_step,6*ch_step) # ch_step=48
