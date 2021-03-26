04da0ab512eed7b3ae087fc2e17abd877ea638c4
rgee3894 <rgga16@gmail.com>
2021-03-26 15:53:07 +0800

    args: removed unused args; added --uv_style_pairs; renamed some args

--- args.py
+++ args.py
@@ -1,25 +1,22 @@
 
 import argparse
 from defaults import DEFAULTS as D
+import datetime
+import os 
 
 
 def parse_arguments():
+    date = datetime.datetime.today().strftime('%m-%d-%y %H-%M-%S')
     parser = argparse.ArgumentParser(
         description='Texture Transfer Algorithm')
-    parser.add_argument('--mesh', type=str, default=D.MESH_PATH(),
-                        help='Path to the mesh OBJ file')
-    parser.add_argument('--style', type=str, default=D.STYLE_PATH(),
-                        help='Path to the style image to transfer texture from')
     parser.add_argument('--style_dir', type=str, default='./inputs/style_images/masked',
-                        help='Path to the style images dir to transfer textures from')
-    parser.add_argument('--content_dir', type=str, default='./inputs/uv_maps/office chair/unwrap',
-                        help='Path to the content images dir to transfer textures onto')                    
-    parser.add_argument('--texture', type=str, default=None,
-                        help='Path to the texture image to transfer texture from')
-    parser.add_argument('--content', type=str, default=None,
-                        help='Path to the content image to transfer texture onto')
-    parser.add_argument('--output_dir', type=str, default='./outputs/output_images',
+                        help='Path to the style images directory to retrieve textures from')
+    parser.add_argument('--uv_dir', type=str, default='./inputs/uv_maps/office chair/unwrap',
+                        help='Path to the uv maps directory of UV maps to transfer textures onto')                    
+    parser.add_argument('--output_dir', type=str, default='./outputs/output_images/[{}]'.format(date),
                         help='Path to the output directory')
+    parser.add_argument('--uv_style_pairs',type=str, 
+                        help='Path to .json file of uv map and style image pairings.')
     parser.add_argument('--epochs', type=int, default=D.EPOCHS(),
                         help='Number of epochs to optimize')
     parser.add_argument('--lr', type=float, default=0.001,
@@ -34,10 +31,8 @@ def parse_arguments():
                         help='Size of output textured uv map')
     parser.add_argument('--style_weight', type=float, default=1e6,
                         help='Style loss weight value')
-    parser.add_argument('--foreground_weight', type=float, default=1e2,
+    parser.add_argument('--foreground_weight', type=float, default=1e5,
                         help='Foreground MSE loss weight value')
-    parser.add_argument('--content_weight', type=float, default=1e2,
-                        help='Content loss weight value')
 
     return parser.parse_args()
 
