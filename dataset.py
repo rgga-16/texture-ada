94f8233ab94bbe93f646773fab73fbdb253cecac
rgee3894 <rgga16@gmail.com>
2021-03-26 15:53:49 +0800

    dataset: minor adjustments

--- dataset.py
+++ dataset.py
@@ -10,9 +10,9 @@ from defaults import DEFAULTS as D
 
 class UV_Style_Paired_Dataset(Dataset):
 
-    def __init__(self, uv_dir,style_dir, uv_sizes, style_size, uv_file, style_file) -> None:
+    def __init__(self, uv_dir,style_dir, uv_sizes, style_size, uv_style_pairs:dict) -> None:
         super().__init__()
-        
+
         self.uv_dir = uv_dir
         self.style_dir = style_dir 
         self.uv_sizes = uv_sizes 
@@ -22,33 +22,17 @@ class UV_Style_Paired_Dataset(Dataset):
 
         # Temporary code. for testing purposes
         ######
-        self.uv_file = 'right_arm_uv.png'
-        self.style_file = 'chair-3_tiled.png'
-        self.uv_files.append(os.path.join(self.uv_dir,uv_file))
-        self.style_files.append(os.path.join(self.style_dir,style_file))
+        # self.uv_file = 'right_arm_uv.png'
+        # self.style_file = 'chair-3_tiled.png'
+        # self.uv_files.append(os.path.join(self.uv_dir,uv_file))
+        # self.style_files.append(os.path.join(self.style_dir,style_file))
         ######
 
-        # lounge sofa
-        # uv_map_style_pairings = {
-        #     # 'tabletop_uv.png':'chair-3_tiled.png',
-        #     # 'botleft_leg_uv.png':'chair-2_tiled.png',
-        #     # 'botright_leg_uv.png':'cobonpue-80_tiled.png',
-        #     # 'topleft_leg_uv.png':'chair-2_tiled.png',
-        #     # 'topright_leg_uv.png':'cobonpue-80_tiled.png',
-        # }
-
-
-        # for k,v in uv_map_style_pairings.items():
-        #     self.uv_files.append(os.path.join(self.uv_dir,k))
-        #     self.style_files.append(os.path.join(self.style_dir,v))
-
-        # for file in os.listdir(self.uv_dir):
-        #     self.uv_files.append(os.path.join(self.uv_dir,file))
-        # for file in os.listdir(self.style_dir):
-        #     self.style_files.append(os.path.join(self.style_dir,file))
+        for k,v in uv_style_pairs.items():
+            self.uv_files.append(os.path.join(self.uv_dir,k))
+            self.style_files.append(os.path.join(self.style_dir,v))
 
         assert len(self.style_files) == len(self.uv_files)
-
     
     def __len__(self):
         return len(self.style_files)
