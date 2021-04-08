ee0ce386627f9ab234d28b0e710335f9720a68f3
rgee3894 <rgga16@gmail.com>
2021-04-08 16:05:26 +0800

    Added adain function

--- losses.py
+++ losses.py
@@ -25,6 +25,40 @@ def gram_matrix(tensor):
     gram = torch.mm(features,features.t())
     return gram / (h*w)
 
+
+def adain(x,y):
+    """
+    Aligns mean and std of feature maps x to that of feature maps y.
+    Based on the AdaIN formula introduced by Huang & Belongie (2017) in their paper,
+    "Arbitrary Style Transfer in Real-time with Adaptive Instance Normalization".
+    Link: https://arxiv.org/pdf/1703.06868.pdf
+
+    :param x: (b,c,h_x,w_x), feature maps x
+    :param x: (b,c,h_y,w_y), feature maps y
+    :return: (b,c,h_x,w_x), feature maps x with mean and variance matching y
+    """
+    b_x,c_x,w_x,h_x = x.shape
+    b_y,c_y,w_y,h_y = y.shape
+
+    assert b_x*c_x == b_y*c_y
+
+    x_feats = x.clone().view(b_x,c_x,w_x*h_x)
+    x_mean = torch.mean(x_feats,dim=2,keepdim=True)
+    x_std = torch.std(x_feats,dim=2,keepdim=True)
+
+    y_feats = y.clone().view(b_y,c_y,w_y*h_y)
+    y_mean = torch.mean(y_feats,dim=2,keepdim=True)
+    y_std = torch.std(y_feats,dim=2,keepdim=True)
+
+    normalized_x = (x_feats - x_mean) / x_std
+    output_feats = (normalized_x * y_std) + y_mean
+
+    output = output_feats.view(b_x,c_x,w_x,h_x)
+    return output
+    
+
+
+
 def covariance_matrix(tensor):
     b,c,w,h = tensor.shape
     feats = tensor.view(b,c,h*w)
