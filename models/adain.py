299ac4c98888b1dcf5fc372dc3719df172a5f712
rgee3894 <rgga16@gmail.com>
2021-04-10 13:46:14 +0800

    Changed style layers and content layers  in getting losses

--- models/adain.py
+++ models/adain.py
@@ -98,6 +98,63 @@ class Network_AdaIN(nn.Module):
         super(Network_AdaIN,self).__init__()
         self.encoder = nn.Sequential(*list(VGG19().features.children())[:21])
 
+        
+        # self.vgg = nn.Sequential(
+        #     nn.Conv2d(3, 3, (1, 1)),
+        #     nn.ReflectionPad2d((1, 1, 1, 1)),
+        #     nn.Conv2d(3, 64, (3, 3)),
+        #     nn.ReLU(),  # relu1-1
+        #     nn.ReflectionPad2d((1, 1, 1, 1)),
+        #     nn.Conv2d(64, 64, (3, 3)),
+        #     nn.ReLU(),  # relu1-2
+        #     nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+        #     nn.ReflectionPad2d((1, 1, 1, 1)),
+        #     nn.Conv2d(64, 128, (3, 3)),
+        #     nn.ReLU(),  # relu2-1
+        #     nn.ReflectionPad2d((1, 1, 1, 1)),
+        #     nn.Conv2d(128, 128, (3, 3)),
+        #     nn.ReLU(),  # relu2-2
+        #     nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+        #     nn.ReflectionPad2d((1, 1, 1, 1)),
+        #     nn.Conv2d(128, 256, (3, 3)),
+        #     nn.ReLU(),  # relu3-1
+        #     nn.ReflectionPad2d((1, 1, 1, 1)),
+        #     nn.Conv2d(256, 256, (3, 3)),
+        #     nn.ReLU(),  # relu3-2
+        #     nn.ReflectionPad2d((1, 1, 1, 1)),
+        #     nn.Conv2d(256, 256, (3, 3)),
+        #     nn.ReLU(),  # relu3-3
+        #     nn.ReflectionPad2d((1, 1, 1, 1)),
+        #     nn.Conv2d(256, 256, (3, 3)),
+        #     nn.ReLU(),  # relu3-4
+        #     nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+        #     nn.ReflectionPad2d((1, 1, 1, 1)),
+        #     nn.Conv2d(256, 512, (3, 3)),
+        #     nn.ReLU(),  # relu4-1, this is the last layer used
+        #     nn.ReflectionPad2d((1, 1, 1, 1)),
+        #     nn.Conv2d(512, 512, (3, 3)),
+        #     nn.ReLU(),  # relu4-2
+        #     nn.ReflectionPad2d((1, 1, 1, 1)),
+        #     nn.Conv2d(512, 512, (3, 3)),
+        #     nn.ReLU(),  # relu4-3
+        #     nn.ReflectionPad2d((1, 1, 1, 1)),
+        #     nn.Conv2d(512, 512, (3, 3)),
+        #     nn.ReLU(),  # relu4-4
+        #     nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+        #     nn.ReflectionPad2d((1, 1, 1, 1)),
+        #     nn.Conv2d(512, 512, (3, 3)),
+        #     nn.ReLU(),  # relu5-1
+        #     nn.ReflectionPad2d((1, 1, 1, 1)),
+        #     nn.Conv2d(512, 512, (3, 3)),
+        #     nn.ReLU(),  # relu5-2
+        #     nn.ReflectionPad2d((1, 1, 1, 1)),
+        #     nn.Conv2d(512, 512, (3, 3)),
+        #     nn.ReLU(),  # relu5-3
+        #     nn.ReflectionPad2d((1, 1, 1, 1)),
+        #     nn.Conv2d(512, 512, (3, 3)),
+        #     nn.ReLU()  # relu5-4
+        # )    
+        
         # Download VGG19 normalized.
         for param in self.encoder.parameters():
             param.requires_grad = False
@@ -142,7 +199,7 @@ class Network_AdaIN(nn.Module):
         x_feats = self.encoder(X)
         style_feats = self.encoder(style)
 
-        # x_feats = adaptive_instance_normalization(x_feats,style_feats)
+        x_feats = adaptive_instance_normalization(x_feats,style_feats)
         output = self.decoder(x_feats)
 
         return output
