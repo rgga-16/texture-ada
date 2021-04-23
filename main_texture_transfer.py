b7f219a52aec73426af590101afff71ea9c8907c
rgee3894 <rgga16@gmail.com>
2021-04-23 13:06:35 +0800

    Moved files to archive. renamed to main_texture_transfer.py

--- main_texture_transfer.py
+++ main_texture_transfer.py
@@ -3,6 +3,7 @@ from torch._C import Value
 from torch.utils.data import DataLoader
 
 from args import args
+from seeder import SEED, init_fn
 from dataset import UV_Style_Paired_Dataset
 from defaults import DEFAULTS as D
 from helpers import logger, image_utils 
@@ -34,14 +35,6 @@ def main():
     feat_extractor = VGG19()
     for param in feat_extractor.parameters():
         param.requires_grad = False
-    
-    # Create output folder
-    # This will store the model, output images, loss history chart and configurations log
-    output_folder = args.output_dir
-    try:
-        os.mkdir(output_folder)
-    except FileExistsError:
-        pass
 
     data = json.load(open(args.uv_style_pairs))
     uv_style_pairs = data['uv_style_pairs']
@@ -77,11 +70,20 @@ def main():
     # train_size = int(0.75 * dataset.__len__())
     # valid_size = dataset.__len__() - train_size 
 
+    # Create output folder
+    # This will store the model, output images, loss history chart and configurations log
+    output_folder = args.output_dir
+    try:
+        os.mkdir(output_folder)
+    except FileExistsError:
+        pass
+
     # Setup dataloader for training
-    dataloader = DataLoader(dataset,num_workers=0,)
+    dataloader = DataLoader(dataset,num_workers=0,worker_init_fn=init_fn)
 
     # Training. Returns path of the generator weights.
     gen_path=train_ulyanov_adain(generator=net,feat_extractor=feat_extractor,dataloader=dataloader)
+    
     
     test_files = uv_style_testpairs.items()
 
@@ -98,6 +100,12 @@ def main():
 
         test_ulyanov_adain(net,test_uvs,gen_path,output_path)
 
+    # INSERT RENDERING MODULE HERE
+    #######################################
+
+
+
+    #######################################
     
     # record time elapsed and configurations
     time_elapsed = time.time() - start 
