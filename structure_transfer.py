bfd5346965deb4ba2310e9ef013cd81b4faae2b2
rgee3894 <rgga16@gmail.com>
2021-04-26 00:10:19 +0800

    added notebook version of structure_transfer; minor adjustments

--- structure_transfer.py
+++ structure_transfer.py
@@ -30,7 +30,6 @@ assert args.num_points is not None
 n_points=args.num_points
 
 # Setup dataset
-##########################################################
 dataset= Pix3D(n_points,category='chair')
 train_size, val_size, test_size = round(0.70 * dataset.__len__()),round(0.20 * dataset.__len__()),round(0.10 * dataset.__len__())
 train_dataset, val_dataset, test_dataset = data.random_split(dataset,[train_size,val_size,test_size],
@@ -62,21 +61,16 @@ for epoch in range(n_epochs):
         bs = pointcloud.shape[0]
         pointcloud = pointcloud.to(D.DEVICE())
         image = image.to(D.DEVICE())
-
         optimizer.zero_grad()
-
         output = model(pointcloud)
-
         loss = models.pointnet.pointcloud_autoencoder_loss(output,pointcloud)
         loss.backward()
         optimizer.step()
-
         train_loss+= loss.item() * bs
         train_running_loss+=loss.item()
         if(i%batch_train_chkpt==batch_train_chkpt-1):
             print('[Epoch {} | Train Batch {}/{} ] Loss: {:.3f}'.format(epoch+1,i+1,len(train_loader),train_running_loss/batch_train_chkpt))
             train_running_loss=0.0
-
     model.eval()
     val_running_loss=0.0
     for j,data_ in enumerate(val_loader):
@@ -84,10 +78,8 @@ for epoch in range(n_epochs):
         bs = pointcloud.shape[0]
         pointcloud = pointcloud.to(D.DEVICE())
         image = image.to(D.DEVICE())
-        # Get output
         with torch.no_grad():
             output = model(pointcloud)
-            # Get val loss and other metrics
             loss,f_score,precision,recall = models.pointnet.pointcloud_autoencoder_loss(output,pointcloud,is_eval=True)
         val_loss+= loss.item() * bs
         val_running_loss+=loss.item()
@@ -112,8 +104,7 @@ torch.save(model.state_dict(),gen_path)
 print(f'Final Model saved in {gen_path}\n')
 
 # Visualize test pointclouds
-##########################################################
-pointclouds,images = iter(test_loader).next() #use train_loader to see if network overfits train data
+pointclouds,images = iter(test_loader).next()
 test_pointcloud = pointclouds[:5]
 model.eval()
 with torch.no_grad():
@@ -122,32 +113,3 @@ with torch.no_grad():
 for tp, f in zip(test_pointcloud,final_output):
     visualizer.display_pointcloud(tp)
     visualizer.display_pointcloud(f)
-
-
-
-
-
-##########################################################
-
-# model_path = './inputs/bunny/reconstruction/bun_zipper.ply'
-# # model_path = './inputs/shape_samples/lounge_sofa/archive/model.obj'
-
-# mesh_o3d = o3d.io.read_triangle_mesh(model_path)
-# mesh_o3d.compute_vertex_normals()
-# # o3d.visualization.draw_geometries([mesh_o3d],width=1280,height=820)
-
-# # Sample points using Open3D
-# pcd_uniform = mesh_o3d.sample_points_uniformly(number_of_points=n_points,use_triangle_normal=False,seed=SEED)
-# pcd_poisson = mesh_o3d.sample_points_poisson_disk(number_of_points=n_points,use_triangle_normal=False,seed=SEED)
-# # o3d.visualization.draw_geometries([pcd_poisson],width=1280,height=820)
-
-# # Sample points using Kaolin then loading into an Open3D object
-# verts,faces,_ = model_utils.load_mesh(model_path)
-# pcd_kal = model_utils.mesh_to_pointcloud(verts,faces,n_points)
-# pcd_o3d = model_utils.pointcloud_kaolin_to_open3d(pcd_kal)
-
-# # Poisson Reconstruction
-# mesh_poisson_poisson = model_utils.reconstruct_mesh_poisson(pcd_poisson)
-# o3d.visualization.draw_geometries([pcd_poisson,mesh_poisson_poisson],width=1280,height=820)
-# o3d.io.write_triangle_mesh("reconstructed_pcd_poisson.obj",mesh_poisson_poisson)
-    
