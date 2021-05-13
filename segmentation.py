5c486e9c3bb61a5d3d1685f33ee29890fddc9310
rgee3894 <rgga16@gmail.com>
2021-05-13 12:43:45 +0800

    mass image segmentation

--- segmentation.py
+++ segmentation.py
@@ -1,6 +1,6 @@
-from dextr import segment
+from external_libs.dextr import segment
 
-import helpers.utils as utils
+import helpers.image_utils as utils
 from defaults import DEFAULTS as D
 
 import pathlib as p
@@ -10,20 +10,17 @@ import argparse
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
-        description='UV Map Retrieval. Uses Blender API to obtain the UV maps of meshes')
-    parser.add_argument('--image',type=str,help='Path of image to mask')
-    parser.add_argument('--output',type=str,help='Path to save output masked image')
+        description='Segmentor')
+    parser.add_argument('--image_dir',type=str,help='Path to dir of images to mask')
     
     return parser.parse_args()
 
-def mask(mask_path, image_path,output_path):
+def mask(mask_path, image_path):
     mask = utils.load_image(mask_path,mode="L")
-    style = utils.load_image(image_path,mode="RGBA")
-    output = style.copy()
-    output.putalpha(mask)
-    output.save(output_path,'PNG')
-    return output_path
-
+    image = utils.load_image(image_path,mode="RGBA")
+    masked = image.copy()
+    masked.putalpha(mask)
+    return masked,mask
 def segment_(image_path,device= D.DEVICE()):
     mask_path = segment.segment_points(image_path=image_path, device=device)
     return mask_path
@@ -32,15 +29,27 @@ def segment_(image_path,device= D.DEVICE()):
 if __name__=='__main__':
     device = D.DEVICE()
     args = parse_arguments()
-    image_path = args.image
-    mask_path = segment.segment_points(image_path=image_path, device=device)
-    output_path = args.output
-    mask(mask_path,image_path,output_path)
-    os.remove(mask_path)
 
-    # img = utils.load_image(image_path)
-    # tensor = utils.image_to_tensor(img)
-    # mask = tensor[:,3,...].unsqueeze(0)
-    # mask_img = utils.tensor_to_image(mask,denorm=False)
-    # mask_img.save('uv_map_backseat_chair-2_masked_mask.png')
+    output_folder = os.path.join(args.image_dir, 'masked')
+    try:
+        os.mkdir(output_folder)
+    except FileExistsError:
+        pass
+    
+    mask_folder = os.path.join(args.image_dir, 'masks')
+    try:
+        os.mkdir(mask_folder)
+    except FileExistsError:
+        pass
+
+    for f in os.listdir(args.image_dir):
+        if(not os.path.isfile(os.path.join(args.image_dir,f))):
+            continue
+        image_path = os.path.join(args.image_dir,f)
+        temp_mask_path = segment.segment_points(image_path=image_path, device=device)
+        masked_img, mask_ = mask(temp_mask_path,image_path)
+        masked_img.save(os.path.join(output_folder,f'{f[:-4]}.png'))
+        mask_.save(os.path.join(mask_folder,f'{f[:-4]}.png'))
+        os.remove(temp_mask_path)
+
 
