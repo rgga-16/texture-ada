6c07211ecde049ca6dd186345380c11a945a4860
rgee3894 <rgga16@gmail.com>
2021-05-13 16:24:34 +0800

    added set_seed function

--- seeder.py
+++ seeder.py
@@ -7,15 +7,19 @@ def init_fn(worker_id):
     np.random.seed(int(SEED))
     random.seed(int(SEED))
 
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU.
+    np.random.seed(seed)  # Numpy module.
+    random.seed(seed)  # Python random module.
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+
 
 global SEED 
 SEED=32
-torch.manual_seed(SEED)
-torch.cuda.manual_seed(SEED)
-torch.cuda.manual_seed_all(SEED)  # if you are using multi-GPU.
-np.random.seed(SEED)  # Numpy module.
-random.seed(SEED)  # Python random module.
-torch.manual_seed(SEED)
-torch.backends.cudnn.benchmark = False
-torch.backends.cudnn.deterministic = True
+set_seed(SEED)
+
 
